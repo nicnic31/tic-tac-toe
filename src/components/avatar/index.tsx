6e9b67156bfbf4997fbbdcbe9724ec5ab5de0920
nicnic31 <permejonics@gmail.com>
2023-07-16 00:11:52 +0800

    created playerDetailsModal UI

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,10 +1,19 @@
-export default function Avatar({link} : {link: string}) {
+interface AvatarProps {
+  link: string;
+  width?: number;
+  height?: number;
+}
+export default function Avatar({ link, width = 80, height = 80 }: AvatarProps) {
   return (
     <img
-      src={link}
+      src={
+        link !== ''
+          ? link
+          : "https://static.vecteezy.com/system/resources/previews/020/911/740/non_2x/user-profile-icon-profile-avatar-user-icon-male-icon-face-icon-profile-icon-free-png.png"
+      }
       alt="avatar"
-      width={80}
-      height={80}
+      width={width}
+      height={height}
       className="bg-[#DDDDDD] rounded-full object-contain"
     />
   );
---
+++ src/components/utils/avatars.tsx
@@ -0,0 +1,8 @@
+export const avatars = [
+  "https://api.dicebear.com/6.x/adventurer/svg?seed=Lucy",
+  "https://api.dicebear.com/6.x/adventurer/svg?seed=Casper",
+  "https://api.dicebear.com/6.x/adventurer/svg?seed=Garfield",
+  "https://api.dicebear.com/6.x/adventurer/svg?seed=Misty",
+  "https://api.dicebear.com/6.x/adventurer/svg?seed=Mia",
+  "https://api.dicebear.com/6.x/adventurer/svg?seed=Missy",
+];
